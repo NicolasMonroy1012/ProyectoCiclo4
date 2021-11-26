b3e30386f55c432e8eb2de1049fefeae22016ed9
Nicolas Monroy <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-11-25 19:36:18 -0500

    include swagger

--- src/main/java/com/ProjectCiclo4/Backend/controller/UserController.java
+++ src/main/java/com/ProjectCiclo4/Backend/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 /**
  *
+ * This class allows mapping the entity user
  * @author Nicolas Monroy
  *
  */
@@ -19,23 +20,47 @@ import java.util.Optional;
 @CrossOrigin("*")
 public class UserController {
     @Autowired
+    /**
+     * Instance of UserService class
+     */
     private UserService userService;
 
+    /**
+     * Map the table user and bring all users
+     * @return a list of users in DB
+     */
     @GetMapping("/all")
     public List<User> getAll() {
         return userService.getAll();
     }
 
+    /**
+     * Allows to map a new user registration
+     * @param user
+     * @return user register or the user
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User registrar(@RequestBody User user) {
         return userService.registrar(user);
     }
 
+    /**
+     * Allows to map if an user exist or not in the DB
+     * @param email
+     * @param password
+     * @return the user or user with undefined name
+     */
     @GetMapping("/{email}/{password}")
     public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
         return userService.autenticarUsuario(email, password);
     }
+
+    /**
+     * Allows to map if an email exits or not
+     * @param email
+     * @return true or false
+     */
     @GetMapping("/{email}")
     public boolean existeEmail(@PathVariable("email") String email) {
         return userService.existeEmail(email);

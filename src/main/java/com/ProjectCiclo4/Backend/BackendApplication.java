5695f3a5d4332df74d4171cde57412facbf4763b
Nicolas Monroy <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-11-28 16:44:45 -0500

    re

--- src/main/java/com/ProjectCiclo4/Backend/BackendApplication.java
+++ src/main/java/com/ProjectCiclo4/Backend/BackendApplication.java
@@ -1,28 +1,31 @@
 package com.ProjectCiclo4.Backend;
 
+import com.ProjectCiclo4.Backend.repository.crud.ClothesCRUDRepository;
 import com.ProjectCiclo4.Backend.repository.crud.UserCRUDRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
-	@Autowired
-	private UserCRUDRepository userCRUDRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    @Autowired
+    private UserCRUDRepository userCRUDRepository;
+    @Autowired
+    private ClothesCRUDRepository clothesCRUDRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-		userCRUDRepository.deleteAll();
+    @Override
+    public void run(String... args) throws Exception {
+        userCRUDRepository.deleteAll();
+        clothesCRUDRepository.deleteAll();
 
-		userCRUDRepository.saveAll(List.of(
-
-		));
-	}
+    }
 }
